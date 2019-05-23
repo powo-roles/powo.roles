39a3f729626bc6a68aea1419db9a15df8f984772
Laurent Almeras <lalmeras@gmail.com>
2019-05-23 22:59:58 +0200

    wip to pass arguments to ansible vars

--- clickables.py
+++ clickables.py
@@ -27,10 +27,33 @@ def main(ctx):
     clickable.utils.load_config(ctx, __name__, __file__, 'clickables.yml')
 
 
-def run_playbook_task(click_group, command_name, playbook, host_or_group):
-    clickable_ansible.run_playbook_task(click_group, command_name,
-        playbook, short_help="Deployment on {}".format(host_or_group),
+def run_playbook_task(click_group, command_name, playbook, decorators=[]):
+    host_or_group = 'localhost'
+    clickable_ansible.run_playbook_task(
+        click_group, command_name,
+        playbook, decorators=decorators,
+        short_help="Deployment on {}".format(host_or_group),
         help="Deployment on {}".format(host_or_group),
         common_hosts=host_or_group)
 
-install = run_playbook_task(main, 'install', 'playbooks/install.yml', "localhost")
+
+def prompt_password(ctx, param, value):
+    if value:
+        password = click.prompt('Provides a password for user creation',
+                                hide_input=True, confirmation_prompt=True)
+        ctx.params['password'] = password
+        return password
+
+
+install = run_playbook_task(
+    main, 'install', 'playbooks/install.yml',
+    decorators=[
+        click.option('user', '--user', '-u'),
+        click.option('password', '--password', '-p',
+                     is_flag=True, callback=prompt_password,
+                     expose_value=True),
+        click.option('uid', '--uid', '-i'),
+        click.option('fullname', '--fullname', '-f'),
+        clickable_ansible.kwargs_to_extra_vars('user', 'password', 'uid',
+                                               'fullname')
+    ])
