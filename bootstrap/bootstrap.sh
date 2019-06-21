5478cb4b9a6778c6eb45447a3c43a64da0180ca3
Laurent Almeras <laurent.almeras@kobalt.fr>
2019-06-21 15:46:03 +0200

    Allow branch switch, allow file:/// scheme
    wget does not allow file:/// scheme ; curl allows it.

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,8 +3,8 @@
 THIS_SCRIPT="$( realpath "$0" )"
 THIS_PATH="$( dirname "$THIS_SCRIPT" )"
 : ${BOOTSTRAP_PY_BRANCH:=master}
-: ${BOOTSTRAP_PY:=https://raw.githubusercontent.com/lalmeras/clickable_bootstrap/master/bootstrap.py}
+: ${BOOTSTRAP_PY:=https://raw.githubusercontent.com/lalmeras/clickable_bootstrap/${BOOTSTRAP_PY_BRANCH}/bootstrap.py}
 
 BOOTSTRAP_COMMAND="poetry config settings.virtualenvs.create false && cd '$THIS_PATH' && poetry install -v"
 
-wget -q -O - "${BOOTSTRAP_PY}" | BOOTSTRAP_COMMAND="$BOOTSTRAP_COMMAND" BOOTSTRAP_PATH="$THIS_PATH" python - "$@"
+curl -s -o - "${BOOTSTRAP_PY}" | BOOTSTRAP_COMMAND="$BOOTSTRAP_COMMAND" BOOTSTRAP_PATH="$THIS_PATH" python - "$@"
